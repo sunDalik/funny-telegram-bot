c345f9602c3dacb5dd206a0bd5926fb7113e8e6b
sunDalik <dali1999@mail.ru>
2022-12-10 12:16:05 +0300

    Add announcer util

---
+++ utils/README.md
@@ -0,0 +1,4 @@
+Bot utility scripts  
+
+### announcer.py chatid
+Allows you to send a custom message to chat with id <chatid>
---
+++ utils/announcer.py
@@ -0,0 +1,20 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from _secrets import secrets_bot_token
+from telegram.ext import Updater
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) < 2):
+         print("Provide chat ID as a command line argument")
+         quit()
+    u = Updater(secrets_bot_token, use_context=True)
+    print(f"Enter a message that will be sent to chat {sys.argv[1]}. Press Ctrl+D to send.")
+    
+    message = ""
+    for line in sys.stdin:
+        message += line
+    if (message != ""):
+        u.bot.send_message(chat_id=sys.argv[1], text=message)

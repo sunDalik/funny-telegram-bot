b7e94fcd2f49a216f9c3a212d9470ba616c18bcc
emudumburemu <pralinelol11@gmail.com>
2024-12-17 01:31:42 +0400

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -6,10 +6,13 @@ class MahjongGame:
         self.players = []
         self.wall = Wall()
         self.dora_indicators = []
-    
+        self.honbas = 0
+        self.points = {}
+
     def add_player(self, player_id):
         if len(self.players) < 4:
             self.players.append(Player(player_id))
+            self.points[player_id] = 25000  # Starting points for each player
     
     def ready_to_start(self):
         return len(self.players) == 4
@@ -35,3 +38,16 @@ class MahjongGame:
     def broadcast_new_dora(self, new_dora):
         for player in self.players:
             player.notify_new_dora(new_dora)
+    
+    def end_round(self, winner_id, points_won):
+        for player in self.players:
+            if player.user_id == winner_id:
+                self.points[winner_id] += points_won
+            else:
+                self.points[player.user_id] -= points_won // 3
+        self.honbas += 1
+        self.broadcast_points()
+
+    def broadcast_points(self):
+        for player in self.players:
+            player.notify_points(self.points)
